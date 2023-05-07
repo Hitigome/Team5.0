8834700ccd8457a787e92e5a26050548baa6d5fc
Hitigome <kawibrian1@gmail.com>
2023-05-06 21:33:00 -0700

    Make header its own function

--- team5.0/src/App.js
+++ team5.0/src/App.js
@@ -4,34 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="body">
-      <header>
-        <div className="container">
-          <div className="row">
-            <div className="col">
-              <h3>Site Titles</h3>
-            </div>
-            <div className="col-6"></div>
-            <div className="col">
-              <h3>Profile</h3>
-            </div>
-          </div>
-        </div>
-
-        <div className="row text-center">
-          <div className="col-6 mx-auto">
-            <h2>Shows in Location</h2>
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-6 mx-auto">
-            <form className="form">
-              <input className="form-control" type="search" placeholder="Search by city" aria-label="Search" />
-            </form>
-          </div>
-        </div>
-      </header>
-
+    <div>{header()}</div> 
       <main>
         <div className="container py-3">
           <h4>Your Favorite Artists</h4>
@@ -185,6 +158,38 @@ function App() {
       </main>
 
     </div>
+  );
+}
+
+function header(){
+  return(
+    <header>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h3>Site Titles</h3>
+        </div>
+        <div className="col-6"></div>
+        <div className="col">
+          <h3>Profile</h3>
+        </div>
+      </div>
+    </div>
+
+    <div className="row text-center">
+      <div className="col-6 mx-auto">
+        <h2>Shows in Location</h2>
+      </div>
+    </div>
+
+    <div className="row">
+      <div className="col-md-6 mx-auto">
+        <form className="form">
+          <input className="form-control" type="search" placeholder="Search by city" aria-label="Search" />
+        </form>
+      </div>
+    </div>
+  </header>
   );
 }
 
